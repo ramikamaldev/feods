30d636e92e3ffe3e010f9c07dcf18807f08a573c
Rami Kamal <ramikamal@outlook.com>
2019-11-24 22:07:55 +0400

    changed gruntfile configuration to functionally test Paypal APIs on Azure

--- src/feods-app.ts
+++ src/feods-app.ts
@@ -4,67 +4,66 @@ import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import * as path from "path";
 
-import { connectToFeodsMongodb } from "./common-functions/mongo-connect"
-import { createAndReturnFeodsRouter } from "./server-routes/feods-routes"
+import { connectToFeodsMongodb } from "./common-functions/mongo-connect";
+import { createAndReturnFeodsRouter } from "./server-routes/feods-routes";
 
 class FeodsApp {
     public server: express.Application;
     constructor() {
         this.server = express();
-        dotenv.config({ path: path.resolve(__dirname, "../config/config.env") })
+        dotenv.config({
+            path: path.resolve(__dirname, "../config/config.env"),
+        });
         this.instantiateApplicationInfrastructure();
     }
 
     public async instantiateApplicationInfrastructure() {
-        let result = connectToFeodsMongodb().then(
-            async function (result) {
+        let result = connectToFeodsMongodb()
+            .then(async function(result) {
                 await feodsApp.instantiateMiddleware();
                 feodsApp.startExpress();
-            }).catch(
-                function (error) {
-                    //TODO: If the promise was rejected, throw the error and terminate.
-                    console.log(error)
-                });
+            })
+            .catch(function(error) {
+                //TODO: If the promise was rejected, throw the error and terminate.
+                console.log(error);
+            });
         return;
     }
 
-    public async instantiateMiddleware() {
-        let promiseFunction = (function (resolve, reject) {
+    public async instantiateMiddleware()  {
+        let promiseFunction = function(resolve, reject) {
             //this.server.use(helmet());
-            this.server.use(bodyParser.json({ type: 'application/*+json' }));
+            this.server.use(bodyParser.json({ type: "application/*+json" }));
             let feodsRouter = createAndReturnFeodsRouter();
             this.server.use("/", feodsRouter);
-            this.server.get("/testServerRoutes", function (req, res) {
-                res.send("Server is running correctly.")
+            this.server.get("/testServerRoutes", function(req, res) {
+                res.send("Server is running correctly.");
             });
             return resolve(0);
-        }).bind(this);
+        }.bind(this);
         this.createPromise(promiseFunction);
-
-    };
+    }
 
     public async startExpress() {
         //TODO: set port in dotenv.
-        this.server.listen("1234");
-        console.log("Feods Server Started! Listening on port: 1234");
+        this.server.listen(process.env.PORT);
+        console.log(
+            `Feods Server Started! Listening on port: ${process.env.PORT}`
+        );
     }
 
     public async createPromise(promiseFunction) {
         return new Promise(promiseFunction);
     }
-
 }
-
-
 
 let feodsApp: FeodsApp;
 function createSingletonApplication() {
     if (!feodsApp) {
-        console.log("Instantiating Singleton Feods Application")
+        console.log("Instantiating Singleton Feods Application");
         feodsApp = new FeodsApp();
         return 0;
-    }
-    else {
+    } else {
         return 1;
     }
 }
---
+++ src/feods-functionality/payments/paypal/feods-paypal-intg.ts
@@ -0,0 +1,7 @@
+import * as paypalRestSDK from "paypal-rest-sdk";
+
+function createPaypalPayment() { }
+
+function executePaypalPayment() { }
+
+function retrievePaypalPayment() { }
